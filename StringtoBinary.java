e404b37b3f7c124b2f9174173d0088fdb1e515db
bilalcelebi <51432063+bilalcelebi@users.noreply.github.com>
2020-04-22 19:19:10 +0300

    Add files via upload

---
+++ StringtoBinary.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class StringtoBinary {
+	
+public static void main(String[] args){
+		
+		while(true) {
+			
+			System.out.println("Bir Sözcük Giriniz : ");
+			Scanner scan = new Scanner(System.in);
+			String sozcuk = scan.next();
+			String sonuc = "";
+			
+			for(int i = 0; i < sozcuk.length(); i++) {
+				
+					int harf = sozcuk.charAt(i);
+					String result = toBinary(harf);
+					sonuc += result;
+			}
+			
+			System.out.println("Sonuç : " + sonuc);
+		}
+		
+	}
+
+	public static String toBinary(int sayi) {
+	
+		String binaryconvert = Integer.toBinaryString(sayi);
+	
+		return binaryconvert;
+	}
+}
